e26e5ec508a92cb95ce17bf919e58800a618cfac
nonhlanhlaCindi <nontethelelocindi42@gmail.com>
2025-03-10 11:24:34 +0200

    My services

---
+++ movie-app/src/app/movielist/movielist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movielist',
+  standalone: true,
+  imports: [],
+  templateUrl: './movielist.component.html',
+  styleUrl: './movielist.component.scss'
+})
+export class MovielistComponent {
+
+}
